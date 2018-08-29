d18a997b77415edb6f25a663c61d52167abf251a
Unknown <skyesladeut@outlook.com>
2018-08-28 18:05:29 -0600

    Worked on NavMesh and cameras

---
+++ 2210Fall/Assets/Follow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Follow : MonoBehaviour
+{
+
+    public NavMeshAgent Roamer;
+    public Transform WalkTowards;
+
+    private void Start()
+    {
+        Roamer = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        Roamer.destination = WalkTowards.position;
+    }
+
+}
---
+++ 2210Fall/Assets/RandomWalk.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class RandomWalk : MonoBehaviour
+{
+
+	public float Radius;
+	public float WanderTimer;
+
+	private Transform target;
+	public NavMeshAgent Wanderer;
+	private float timer;
+
+	private void Start()
+	{
+		Wanderer = GetComponent<NavMeshAgent>();
+		timer = WanderTimer;
+	}
+
+	private void Update()
+	{
+		timer += Time.deltaTime;
+		if (timer >= WanderTimer)
+		{
+			Vector3 newPos = RandomNavSphere(transform.position, Radius, -1);
+			Wanderer.SetDestination(newPos);
+			timer = 0;
+		}
+	}
+
+	public Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+	{
+		Vector3 direction = Random.insideUnitSphere * dist;
+		direction += origin;
+		NavMeshHit navHit;
+		NavMesh.SamplePosition(direction, out navHit, dist, layermask);
+		return navHit.position;
+	}
+}
