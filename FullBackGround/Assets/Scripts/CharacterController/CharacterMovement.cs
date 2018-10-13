92ee8198f7205623db55447233f8d87d398a9001
Unknown <skyesladeut@outlook.com>
2018-10-12 18:31:36 -0600

    Amusement park grounds

---
+++ FullBackGround/Assets/Scripts/CharacterController/CharacterMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMovement : MonoBehaviour
+{
+
+    public MovePattern movePattern;
+    private MovePattern Current;
+    private CharacterController controller;
+
+
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Current = movePattern;
+    }
+
+    private void Update() {
+        Current.Invoke(controller, transform);
+        
+
+    }
+
+
+
+}
