0e103093373e34ff2e7c0cd1c597541a1f55ccc9
Unknown <skyesladeut@outlook.com>
2018-10-17 14:38:41 -0600

    FairGrounds

---
+++ FullBackGround/Assets/Scripts/OnTriggerEnterEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnTriggerEnterEvent : MonoBehaviour
+{
+
+	public UnityEvent Event;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("Player"))
+			Event.Invoke();
+	}
+}
